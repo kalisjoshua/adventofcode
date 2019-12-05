d29ac5b31d547d63de408388f5dc8be2d6f8c366
Joshua T Kalis <kalisjoshua@gmail.com>
2019-12-04 21:03:20 -0500

    solved day 2 part 2

--- src/day02.js
+++ src/day02.js
@@ -1,35 +1,42 @@
 const input = process.argv[2]
   .match(/\d+/g)
 
-input[1] = "12"
-input[2] = "2"
-
-const computor = {
+const operations = {
   1: (a, b) => a + b,
   2: (a, b) => a * b,
 }
 
-let offset = 0
+function compute (noun, verb) {
+  let program = input.slice(0)
 
-processing:
-// eslint-disable-next-line no-constant-condition
-while (true) {
-  const [opCode, nounIndex, verbIndex, outputIndex] = input
-    .slice(offset)
+  program[1] = noun
+  program[2] = verb
 
-  if (/^99$/.test(opCode)) {
-    break processing
-  } else if (opCode in computor) {
-    // if (/^19690720$/.test(100 * noun + verb)) {
-    //   throw new Error("FOUND!")
-    //   break processing
-    // }
-    input[outputIndex] = computor[opCode](+input[nounIndex], +input[verbIndex])
-    offset = offset + 4
-  } else {
-    throw new Error(`Invalid op code: ${opCode}`)
+  let offset = 0
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const [opCode, a, b, i] = program
+      .slice(offset)
+
+    if (/^99$/.test(opCode)) {
+      return program[0]
+    } else if (opCode in operations) {
+      program[i] = operations[opCode](+program[a], +program[b])
+      offset = offset + 4
+    } else {
+      throw new Error(`Invalid op code: ${opCode}`)
+    }
   }
 }
 
+let noun = 12
+let verb = 2
+const target = 19690720
+
+while (target > compute(++noun, verb));
+--noun
+while (target > compute(noun, ++verb));
+
 // eslint-disable-next-line no-console
-console.log(input[0])
+console.log(100 * noun + verb)

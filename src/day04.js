ee92eff664cef8e66d6e789999d54f640ef8614a
Joshua T Kalis <kalisjoshua@gmail.com>
2019-12-06 12:25:50 -0500

    day 4 complete

---
+++ src/day04.js
@@ -0,0 +1,18 @@
+const input = process.argv[2]
+  .trim()
+  .split("-")
+
+let [min] = input
+const options = []
+
+const alwaysIncreasing = (num) =>
+  num == num.toString().split("").sort().join("")
+const properSequence = (num) =>
+  (num.toString().match(/(\d)\1+/g) || []).map((s) => s.length).join("").includes(2)
+
+do {
+  if (properSequence(min) && alwaysIncreasing(min)) options.push(min)
+} while (++min < input[1])
+
+// eslint-disable-next-line no-console
+console.log("Total options", options.length)

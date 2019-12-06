c02bcad3fd0404596b56d85febfc7ee5cd6d92c3
Joshua T Kalis <kalisjoshua@gmail.com>
2019-12-06 14:58:49 -0500

    refactor for reuse

--- src/day02.js
+++ src/day02.js
@@ -1,42 +1,24 @@
+const computor = require("../lib/computor")
+
 const input = process.argv[2]
   .match(/\d+/g)
-
-const operations = {
-  1: (a, b) => a + b,
-  2: (a, b) => a * b,
-}
-
-function compute (noun, verb) {
-  let program = input.slice(0)
-
-  program[1] = noun
-  program[2] = verb
-
-  let offset = 0
-
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    const [opCode, a, b, i] = program
-      .slice(offset)
-
-    if (/^99$/.test(opCode)) {
-      return program[0]
-    } else if (opCode in operations) {
-      program[i] = operations[opCode](+program[a], +program[b])
-      offset = offset + 4
-    } else {
-      throw new Error(`Invalid op code: ${opCode}`)
-    }
-  }
-}
 
 let noun = 12
 let verb = 2
 const target = 19690720
 
-while (target > compute(++noun, verb));
+const fixInput = (input, n, v) => {
+  const changed = input.slice(0)
+
+  changed[1] = n
+  changed[2] = v
+
+  return changed
+}
+
+while (target > computor(fixInput(input, ++noun, verb)));
 --noun
-while (target > compute(noun, ++verb));
+while (target > computor(fixInput(input, noun, ++verb)));
 
 // eslint-disable-next-line no-console
 console.log(100 * noun + verb)

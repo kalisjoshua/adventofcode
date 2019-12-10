4b5dbf1dd2cb6e70dd031ca0f47dc2bbe723c7ff
Joshua T Kalis <kalisjoshua@gmail.com>
2019-12-09 20:53:32 -0500

    I hope this works

--- src/day05.js
+++ src/day05.js
@@ -7,6 +7,6 @@ const input = process.argv[2]
 // console.log(Math.random())
 // console.log(computor(input))
 
-console.log(computor(input))
+computor(input)
 // console.log(computor("1002,4,3,4,33".match(/\d+/g)))
-// computor("3,0,4,0,1,1,3,2,4,2,99".match(/\d+/g))
+// computor("3,0,102,7,6,0,4,0,99".match(/\d+/g))

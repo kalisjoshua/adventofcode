be7d5a7393617b2069d176fe097b4dc48d4a693c
Joshua T Kalis <kalisjoshua@gmail.com>
2019-12-28 18:44:27 -0500

    refactor day 7 part 1

--- src/day07.js
+++ src/day07.js
@@ -1,51 +1,57 @@
 const computorFactory = require("../lib/reload.js")("../lib/computor.js")
-
 const permute = require("../lib/permute")
+const pipeline = require("../lib/pipeline")
 const range = require("../lib/range")
 
 // eslint-disable-next-line no-console
 const log = (...args) => console.log(...args)
-// eslint-disable-next-line no-constant-condition
-const debug = false ? (...args) => log("DEBUG", ...args) : () => {}
+// // eslint-disable-next-line no-constant-condition
+// const debug = false
+//   ? (...args) => log("DEBUG", ...args)
+//   : () => {}
 
-function maxOutput (program) {
-  const all = Array.from(permute(range(5, -1)))
-    .map((phases) => runPhaseSetting(phases, program.match(/-?\d+/g).slice(0)))
-
-  return Math.max.apply(null, all)
-}
-
-function runPhaseSetting (phases, memory) {
+function phaseSetting (phases, memory) {
   return phases
     .reduce((ampInput, phase) => {
       const ar = [phase, ampInput]
       const get = () => ar.shift()
       const puts = (val) => puts.val = [...(puts.val || []), val]
-      computorFactory(get, puts, debug)(memory.slice(0))
+      computorFactory(get, puts)(memory.slice(0))
 
       return puts.val.pop()
     }, 0)
 }
 
-function runProgram (input, expected) {
-  runProgram.run = (runProgram.run || 0) + 1
+function run (program, expected) {
+  run.run = (run.run || 0) + 1
+  program = program
+    .match(/-?\d+/g)
+    .slice(0)
 
-  const result = maxOutput(input)
+  const maxThrusterOutput = pipeline(
+    range(5, -1),
+    permute,
+    Array.from,
+    (permutations) => permutations.map((phases) => phaseSetting(phases, program)),
+    (outputs) => Math.max.apply(null, outputs)
+  )
 
   if (expected) {
-    if (result != expected) {
-      runProgram.anyFailing = true
-      log(`${runProgram.run} - ${result} should be ${expected}`)
+    if (maxThrusterOutput != expected) {
+      run.anyFailing = true
+      log(`${run.run} - ${maxThrusterOutput} should be ${expected}`)
     }
   } else {
-    if (!runProgram.anyFailing) {
-      log("try this", result)
+    if (!run.anyFailing) {
+      log("try this", maxThrusterOutput)
     }
   }
 }
 
-runProgram(`3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0`, 43210)
-runProgram(`3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0`, 54321)
-runProgram(`3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0`, 65210)
+run(`3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0`, 43210)
+run(`3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0`, 54321)
+run(`3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0`, 65210)
 
-runProgram(process.argv[2])
+run(process.argv[2], 366376)
+
+if (!run.anyFailing) log("Program completed successfully")

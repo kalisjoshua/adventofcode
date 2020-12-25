9e00eb59cf57af81e35b10dec9406e428484312b
Joshua T Kalis <kalisjoshua@gmail.com>
2020-12-25 06:56:43 -0500

    not going to get very far on this today

---
+++ 2020/day25.js
@@ -0,0 +1,25 @@
+const cleanInput = (input) => input
+  .trim()
+  .split(/\n/)
+
+function main (input, libs) {
+  input = cleanInput(input)
+
+  main.libs = libs
+
+  partOne(input, (...args) => libs.report('Part one', ...args))
+  partTwo(input, (...args) => libs.report('Part two', ...args))
+}
+
+function partOne (input, report) {
+
+  report()
+}
+
+function partTwo (input, report) {
+  // const result = input
+
+  report()
+}
+
+module.exports = main

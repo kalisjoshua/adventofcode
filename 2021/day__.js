1d22a4266ae51115f7afc2b19c28107b004d4c0d
Joshua T Kalis <kalisjoshua@gmail.com>
2021-12-05 14:29:27 -0500

    simple template

---
+++ 2021/day__.js
@@ -0,0 +1,14 @@
+const example = `
+
+`
+
+module.exports = (input, {report}) => {
+  input = input.trim().split(/\n/)
+
+  const partOne = input
+
+  const partTwo = input
+
+  // report('Part one', partOne)
+  // report('Part two', partTwo)
+}

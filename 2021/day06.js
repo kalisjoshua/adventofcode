a5bc73542d231b5de405340931d23d8ac1c4774b
Joshua T Kalis <kalisjoshua@gmail.com>
2021-12-06 14:41:40 -0500

    still working on my own idea for a solution

--- 2021/day06.js
+++ 2021/day06.js
@@ -1,31 +1,31 @@
 const example = `3,4,3,1,2`
 
+// source: https://www.reddit.com/r/adventofcode/comments/r9z49j/comment/hngsh82/?utm_source=share&utm_medium=web2x&context=3
+function sourcedSolution (fish, days) {
+  const list = fish
+    .reduce((counts, startingAge) => {
+      ++counts[startingAge]
+
+      return counts
+    }, new Array(9).fill(0))
+
+  while (days--){
+    const num = list.shift()
+
+    list[6] += list[8] = num
+  }
+
+  return list
+    .reduce((a, b) => a + b)
+}
+
 module.exports = (input, {report}) => {
   input = input.trim().split(",").map(Number)
 
-  const DAYS = 80
+  const partOne = sourcedSolution(input, 80)
 
-  const generate = (startingAge) => {
-    let day = 0
-    let fish = [startingAge]
-
-    while (DAYS > day++) {
-      fish = fish
-        .concat(fish.filter((age) => age === 0).map(() => 9))
-        .map((age) => age === 0 ? 6 : age - 1)
-    }
-
-    return fish
-  }
-  const table = [1, 2, 3, 4, 5]
-    .reduce((acc, num) => ({...acc, [num]: generate(num).length}), {})
-
-  const partOne = input
-    .map((num) => table[num])
-    .reduce((a, b) => a + b)
-
-  const partTwo = input
+  const partTwo = sourcedSolution(input, 256)
 
   report('Part one', partOne, 379414)
-  // report('Part two', partTwo)
+  report('Part two', partTwo)
 }

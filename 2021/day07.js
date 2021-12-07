dba52002aafdf82d5e89b66e5d5db8947ed5a7ee
Joshua T Kalis <kalisjoshua@gmail.com>
2021-12-07 13:42:27 -0500

    finished and I understand why it works

--- 2021/day07.js
+++ 2021/day07.js
@@ -1,27 +1,20 @@
 const example = `16,1,2,0,4,2,7,1,2,14`
 
-function find (list) {
-  const empty = Array(Math.max(...list)).fill(0)
-  const counts = list
-    .reduce((counts, num) => (counts[num - 1] += 1, counts), empty)
-    .map((count, index) => [count, index + 1])
-    .filter(([count]) => count > 2)
-    // .sort((a, b) => b[0] - a[0])
-    .sort((a, b) => (b[0] * b[1]) - (a[0] * a[1]))
-
-  console.log(counts)
-
-  return counts
-    .map(([_, num]) => num)
-}
-
 module.exports = (input, {report}) => {
   input = input.trim().split(",").map(Number)
 
-  const partOne = find(input)
+  const sorted = input.sort((a, b) => a - b)
+  const mean = Math.floor(input.reduce((a, b) => a + b) / input.length)
+  const midpoint = Math.floor(input.length / 2)
+  const median = Math.floor((sorted[midpoint] + sorted[midpoint]) / 2)
+
+  const partOne = input
+    .reduce((acc, pos) => acc + Math.abs(median - pos), 0)
 
   const partTwo = input
+    .reduce((acc, pos) => acc + Array(Math.abs(mean - pos)).fill()
+      .reduce((a, _, i) => a + i + 1, 0), 0)
 
-  report('Part one', partOne)
-  // report('Part two', partTwo)
+  report('Part one', partOne, 339321)
+  report('Part two', partTwo, 95476244)
 }

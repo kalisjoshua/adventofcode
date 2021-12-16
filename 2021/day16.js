46f315b5932783e8c7b3a4a92d854c5fd0db187a
Joshua T Kalis <kalisjoshua@gmail.com>
2021-12-16 18:05:02 -0500

    really set myself up for a win in part 1

--- 2021/day16.js
+++ 2021/day16.js
@@ -6,6 +6,16 @@
 // const example = "C0015000016115A2E0802F182340"   // version sum 23
 // const example = "A0016C880162017C3686B18A3D4780" // version sum 31
 
+// C200B40A82 finds the sum of 1 and 2, resulting in the value 3.
+// 04005AC33890 finds the product of 6 and 9, resulting in the value 54.
+// 880086C3E88112 finds the minimum of 7, 8, and 9, resulting in the value 7.
+// CE00C43D881120 finds the maximum of 7, 8, and 9, resulting in the value 9.
+// D8005AC2A8F0 produces 1, because 5 is less than 15.
+// F600BC2D8F produces 0, because 5 is not greater than 15.
+// 9C005AC2F8F0 produces 0, because 5 is not equal to 15.
+// 9C0141080250320F1802104A08 produces 1, because 1 + 3 = 2 * 2.
+const example = "9C0141080250320F1802104A08"
+
 const LITERAL_VALUE_PACKET_TYPE_ID = 4
 const TYPE_LENGTH = 3
 const VERSION_LENGTH = 3
@@ -13,6 +23,59 @@ const VERSION_LENGTH = 3
 const sumVersionNumbers = ({subpackets = [], version}) => version + subpackets
   .map(sumVersionNumbers)
   .reduce((a, b) => a + b, 0)
+
+function evaluator (tree) {
+  if (tree.value) return tree.value
+
+  const subpackets = tree.subpackets
+    .map(evaluator)
+
+  switch (tree.operator) {
+    // 0 - sum
+    //    their value is the sum of the values of their sub-packets. If they
+    //    only have a single sub-packet, their value is the value of the
+    //    sub-packet.
+    case 0:
+      return subpackets
+        .reduce((a, b) => a + b, 0)
+    // 1 - product
+    //    their value is the result of multiplying together the values of their
+    //    sub-packets. If they only have a single sub-packet, their value is the
+    //    value of the sub-packet.
+    case 1:
+      return subpackets
+        .reduce((a, b) => a * b, 1)
+    // 2 - minimum
+    //    their value is the minimum of the values of their sub-packets.
+    case 2:
+      return Math.min(...subpackets)
+    // 3 - maximum
+    //    their value is the maximum of the values of their sub-packets.
+    case 3:
+      return Math.max(...subpackets)
+    // 5 - greater than
+    //    their value is 1 if the value of the first sub-packet is greater than
+    //    the value of the second sub-packet; otherwise, their value is 0. These
+    //    packets always have exactly two sub-packets.
+    case 5:
+      return subpackets
+        .reduce((a, b) => a > b ? 1 : 0)
+    // 6 - less than
+    //    their value is 1 if the value of the first sub-packet is less than the
+    //    value of the second sub-packet; otherwise, their value is 0. These
+    //    packets always have exactly two sub-packets.
+    case 6:
+      return subpackets
+        .reduce((a, b) => a < b ? 1 : 0)
+    // 7 - equal to
+    //    their value is 1 if the value of the first sub-packet is equal to the
+    //    value of the second sub-packet; otherwise, their value is 0. These
+    //    packets always have exactly two sub-packets.
+    case 7:
+      return subpackets
+        .reduce((a, b) => a === b ? 1 : 0)
+  }
+}
 
 function literalValueParser (version, body) {
   let acc = ""
@@ -79,12 +142,11 @@ module.exports = (input, {report}) => {
     .join("")
 
   const parsed = parserFn(input)
-  // console.log(JSON.stringify(parsed, null, 4))
 
   const partOne = sumVersionNumbers(parsed)
 
-  // const partTwo = input
+  const partTwo = evaluator(parsed)
 
   report('Part one', partOne, 920)
-  // report('Part two', partTwo)
+  report('Part two', partTwo, 10185143721112)
 }

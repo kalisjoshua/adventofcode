23f15a05455c3637a93e7e4f7f4d16bf2d5a18e2
Joshua T Kalis <kalisjoshua@gmail.com>
2022-12-03 12:10:16 -0500

    day 2 completed

--- 2022/day02.js
+++ 2022/day02.js
@@ -2,36 +2,19 @@ const cleanRawInput = (raw) => raw
   .trim()
   .split(/\n/)
 
-/*
-  A|X / 1 Rock
-  B|Y / 2 Paper
-  C|Z / 3 Scissor
-*/
-
-const playPoints = ['X', 'Y', 'Z']
-const tieConditions = ['A X', 'B Y', 'C Z']
-const winConditions = ['A Y', 'B Z', 'C X']
-
-const key = [
-  'A X',
-  'A Y',
-  'A Z',
-  'B X',
-  'B Y',
-  'B Z',
-  'C X',
-  'C Y',
-  'C Z',
-].reduce((acc, play) => {
-  acc[play] = 1
-    + playPoints.indexOf(play.slice(-1))
-    + (tieConditions.includes(play) ? 3 : 0)
-    + (winConditions.includes(play) ? 6 : 0)
-
-  return acc
-}, {})
-
 function partOne(input, report, answer) {
+  const key = {
+    'A X': 4,
+    'A Y': 8,
+    'A Z': 3,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 7,
+    'C Y': 2,
+    'C Z': 6,
+  }
+
   const result = input
     .reduce((acc, play) => acc + key[play], 0)
 
@@ -39,19 +22,27 @@ function partOne(input, report, answer) {
 }
 
 function partTwo(input, report, answer) {
+  const key = {
+    'A X': 3, // L 0 + 3
+    'A Y': 4, // T 3 + 1
+    'A Z': 8, // W 6 + 2
+    'B X': 1, // L 0 + 1
+    'B Y': 5, // T 3 + 2
+    'B Z': 9, // W 6 + 3
+    'C X': 2, // L 0 + 2
+    'C Y': 6, // T 3 + 3
+    'C Z': 7, // W 6 + 1
+  }
+
   const result = input
+    .reduce((acc, play) => acc + key[play], 0)
 
   report('Part one', result, answer)
 }
 
 module.exports = (raw, { report }) => {
   const input = cleanRawInput(raw)
-  // const input = [
-  //   'A Y',
-  //   'B X',
-  //   'C Z',
-  // ]
 
   partOne(input, report, 13682)
-  // partTwo(input, report, null)
+  partTwo(input, report, 12881)
 }

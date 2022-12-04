7e2875c29c6b99fe40cca7c7521b8b07dc7e1115
Joshua T Kalis <kalisjoshua@gmail.com>
2022-12-04 09:41:35 -0500

    day 4 completed

---
+++ 2022/day04.js
@@ -0,0 +1,33 @@
+const NOPE = Symbol('NOPE')
+const cleanRawInput = (raw) => raw
+  .trim()
+  .split(/\n/)
+  .map((line) => line.match(/\d+/g).map((n) => +n))
+
+function partOne(input, report, answer) {
+  const result = input
+    .filter(([a, b, c, d]) => ((a >= c && b <= d) || (c >= a && d <= b)))
+    .length
+
+  report('Part one', result, answer)
+}
+
+function partTwo(input, report, answer) {
+  const result = input
+    .filter(([a, b, c, d]) => (
+      (a >= c && a <= d) ||
+      (b >= c && b <= d) ||
+      (c >= a && c <= b) ||
+      (d >= a && d <= b)
+    ))
+    .length
+
+  report('Part one', result, answer)
+}
+
+module.exports = (raw, { report }) => {
+  const input = cleanRawInput(raw)
+
+  partOne(input, report, 560)
+  partTwo(input, report, 839)
+}

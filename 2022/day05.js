c834bf7ecf95170bcf8374389d2eaf8b918e7e64
Joshua T Kalis <kalisjoshua@gmail.com>
2022-12-05 14:26:30 -0500

    day 5 completed

--- 2022/day05.js
+++ 2022/day05.js
@@ -12,10 +12,10 @@ const cleanRawInput = (raw) => raw
 
     return acc
   }, [[]])
-
-// const pretty = (stacks) => stacks
-//   .map((stack, index) => `${index + 1} - ${stack.reverse().join(' ')}`)
-//   .join('\n')
+const copy = (obj) => JSON.parse(JSON.stringify(obj))
+const pretty = (stacks) => stacks
+  .map((stack, index) => `${index + 1} - ${stack.slice(0).reverse().join(' ')}`)
+  .join('\n')
 
 function parseInput (raw) {
   const input = cleanRawInput(raw)
@@ -24,8 +24,7 @@ function parseInput (raw) {
     .map((line) => line.match(/\d+/g))
 
   const stacks = input[0]
-    .flatMap((line) => {
-      const result = line
+    .flatMap((line) => line
         .split('')
         .reduce((acc, c, i) => {
           if (/\w/.test(c)) {
@@ -33,9 +32,7 @@ function parseInput (raw) {
           }
 
           return acc
-        }, [])
-      return result
-    })
+        }, []))
     .reduce((acc, [label, stack]) => {
       if (!acc[stack]) {
         acc[stack] = []
@@ -46,11 +43,11 @@ function parseInput (raw) {
       return acc
     }, [])
 
-  return { moves, stacks }
+  return {moves, stacks}
 }
 
 function partOne(input, report, answer) {
-  const {moves, stacks} = input
+  const {moves, stacks} = copy(input)
   const result = moves
     .reduce((stacks, [count, from, to]) => {
       while (count--) {
@@ -66,37 +63,22 @@ function partOne(input, report, answer) {
 }
 
 function partTwo(input, report, answer) {
-  const {moves, stacks} = input
-  console.log(stacks)
-  // console.log(pretty(stacks), '\n')
-  // const result = moves
-  //   .slice(0, 1)
-  //   .reduce((stacks, [count, from, to]) => {
-  //     stacks[to - 1].unshift(...stacks[from - 1].splice(0, count))
+  const {moves, stacks} = copy(input)
+  const result = moves
+    .reduce((stacks, [count, from, to]) => {
+      stacks[to - 1].unshift(...stacks[from - 1].splice(0, count))
 
-  //     console.log({count, from, to})
-  //     console.log(pretty(stacks), '\n')
+      return stacks
+    }, stacks)
+    .map(([top]) => top)
+    .join('')
 
-  //     return stacks
-  //   }, stacks)
-  //   .map(([top]) => top)
-  //   .join('')
-
-  // report('Part one', result, answer)
+  report('Part one', result, answer)
 }
 
 module.exports = (raw, { report }) => {
   const input = parseInput(raw)
-  const example = parseInput(`    [D]
-[N] [C]
-[Z] [M] [P]
- 1   2   3
-
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2`)
 
   partOne(input, report, 'TGWSMRBPN')
-  partTwo(input, report, NOPE)
+  partTwo(input, report, 'TZLTLWRNF')
 }

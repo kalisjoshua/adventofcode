dde5c1a7da4cc0e3a047c7f5640c68f52cd4f870
Joshua T Kalis <kalisjoshua@gmail.com>
2023-12-01 13:29:17 -0500

    day 1 complete

--- 2023/day01.js
+++ 2023/day01.js
@@ -1,14 +1,50 @@
 const NOPE = Symbol("NOPE");
 const cleanRawInput = (raw) => raw.trim().split(/\n/);
 
+const numbers = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+const rNumbers = new RegExp(`^(?:${numbers.join("|")})`);
+
+const solution = (input) =>
+  input
+    .map((line) => line.match(/\d/g) || [])
+    .map((list) => [list.at(0), list.at(-1)].join(""))
+    .map(Number)
+    .reduce((a, b) => a + b);
+
+function parse(str) {
+  const result = str.split("").reduceRight((acc, char, index) => {
+    const [found] = str.slice(index).match(rNumbers) ?? [];
+
+    if (found) {
+      return `${numbers.indexOf(found) + 1}` + acc;
+    } else if (!isNaN(+char)) {
+      return char + acc;
+    }
+
+    return acc;
+  }, "");
+
+  return result;
+}
+
 function partOne(input, report, answer) {
-  const result = input;
+  const result = solution(input);
 
   report("Part one", result, answer);
 }
 
 function partTwo(input, report, answer) {
-  const result = input;
+  const result = solution(input.map(parse));
 
   report("Part two", result, answer);
 }
@@ -16,6 +52,6 @@ function partTwo(input, report, answer) {
 module.exports = (raw, { report }) => {
   const input = cleanRawInput(raw);
 
-  partOne(input, report, NOPE);
-  // partTwo(input, report, NOPE)
+  partOne(input, report, 54968);
+  partTwo(input, report, 54094);
 };
